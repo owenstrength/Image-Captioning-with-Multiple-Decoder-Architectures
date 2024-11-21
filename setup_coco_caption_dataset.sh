dd8f93062ba1cfdc6dcbfbfbe1ad69383f80674e
Jacob Simmons <jakerstx@gmail.com>
2024-11-20 22:04:34 -0600

    update setup shell script

--- setup_coco_caption_dataset.sh
+++ setup_coco_caption_dataset.sh
@@ -6,21 +6,21 @@ cd coco_dataset
 
 # Download train2017 images (13GB)
 echo "Downloading train2017 images..."
-wget http://images.cocodataset.org/zips/train2017.zip
+curl -# -O http://images.cocodataset.org/zips/train2017.zip
 echo "Extracting train2017 images..."
 unzip train2017.zip
 rm train2017.zip
 
 # Download val2017 images (6GB)
 echo "Downloading val2017 images..."
-wget http://images.cocodataset.org/zips/val2017.zip
+curl -# -O http://images.cocodataset.org/zips/val2017.zip
 echo "Extracting val2017 images..."
 unzip val2017.zip
 rm val2017.zip
 
 # Download captions annotations for train and val
 echo "Downloading captions annotations..."
-wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip
+curl -# -O http://images.cocodataset.org/annotations/annotations_trainval2017.zip
 echo "Extracting annotations..."
 unzip annotations_trainval2017.zip
 rm annotations_trainval2017.zip
